261115e07d87cb275f686b6fbd822c975fe64100
Ala Shaabana <ala@bittensor.com>
2023-06-12 11:54:16 -0400

    Added fixes to autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -12,6 +12,21 @@ then
     exit 1
 fi
 
+# Checks if $1 is smaller than $2
+# If $1 is smaller than or equal to $2, then true. 
+# else false.
+versionLessThanOrEqual() {
+    [  "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
+}
+
+# Checks if $1 is smaller than $2
+# If $1 is smaller than $2, then true. 
+# else false.
+versionLessThan() {
+    [ "$1" = "$2" ] && return 1 || versionLessThanOrEqual $1 $2
+}
+
+
 # Parse command line arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
@@ -48,48 +63,45 @@ echo "${args[@]}"
 pm2 start "$script" --name $proc_name --interpreter python3 -- "${args[@]}"
 
 while true; do
-    # Fetch the latest changes from the repository
-    git fetch origin $branch
+    # Fetch the latest tags (if any) from the repository
+    git fetch --all
+
+    # Pull latest changes
     git pull origin $branch
 
-    latest_tag=$(git describe --tags --abbrev=0)
+    latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
 
     echo "current validator tag:" "$current_tag" 
     echo "latest validator tag:" "$latest_tag" 
 
     # If the file has been updated
-    if [ $(printf "%s\n" "$latest_tag" "$current_tag" | sort -V -r | head -1) = $"latest_tag" ]; then
-        if [ "$latest_tag" = "$current_tag" ]; then
-            # tags are the same, no change.
-            echo ""
-        else
-            # latest_tag is newer than current_tag, should download and reinstall.
-            echo "New tag published. Updating the local copy."
-            
-            # Pull latest changes
-            git pull origin $branch
+    if versionLessThan $current_tag $latest_tag; then
+        # latest_tag is newer than current_tag, should download and reinstall.
+        echo "New tag published. Updating the local copy."
+        
+        # Pull latest changes
+        git checkout -b $latest_tag $latest_tag
 
-            # Install latest changes just in case.
-            pip install -e ../
+        # Install latest changes just in case.
+        #pip install -e ../
 
-            # Update the current tag 
-            current_tag=$(git describe --tags --abbrev=0)
-            # Check if script is already running with pm2
-            if pm2 status | grep -q $proc_name; then
-                echo "The script is already running with pm2. Stopping and restarting..."
-                pm2 delete $proc_name
-            fi
+        # Update the current tag 
+        # current_tag=$(git describe --tags --abbrev=0)
+        # # Check if script is already running with pm2
+        # if pm2 status | grep -q $proc_name; then
+        #     echo "The script is already running with pm2. Stopping and restarting..."
+        #     pm2 delete $proc_name
+        # fi
 
-            # Run the Python script with the arguments using pm2
-            echo "Running $script with the following arguments with pm2:"
-            echo "${args[@]}"
-            pm2 start "$script" --name $proc_name --interpreter python3 -- "${args[@]}"
-
-            echo ""
-        fi
+        # # Run the Python script with the arguments using pm2
+        # echo "Running $script with the following arguments with pm2:"
+        # echo "${args[@]}"
+        # pm2 start "$script" --name $proc_name --interpreter python3 -- "${args[@]}"
+        current_tag=$(git describe --tags --abbrev=0)
+        echo ""
     else
         # current tag is newer than the latest on git. This is likely a local copy, so do nothing. 
-        echo "Current tag is newer than the git copy. Will not update."
+        echo "Current tag is newer than or same as the git copy. Will not update."
     fi
 
     # Wait for a while before the next check
