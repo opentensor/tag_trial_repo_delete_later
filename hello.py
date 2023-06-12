9eae49029ec9bcbeca7ef40944cdd9841a127009
Ala Shaabana <ala@bittensor.com>
2023-06-12 11:45:02 -0400

    Updated hello.py

--- hello.py
+++ hello.py
@@ -2,5 +2,5 @@ import time
 
 while True:
 	time.sleep(2)
-	print("Woke up, back to sleep, anotehr change, this is for tag 1.0.6")
+	print("Woke up, back to sleep, anotehr change, this is for tag 1.0.7")
 
